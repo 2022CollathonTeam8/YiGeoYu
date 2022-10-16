c05ee58023d4c3c40b2b9d1235b577a70ccf54b6
YimKeul <leesjun29@gmail.com>
2022-10-16 13:09:43 +0900

    feat : imagepicker

--- src/Screnns/Home/MakeItem.js
+++ src/Screnns/Home/MakeItem.js
@@ -1,10 +1,31 @@
-import * as React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Button, Image, View, Platform } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 
 const MakeItem = () => {
+  const [image, setImage] = useState(null);
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>MakeItem Screen</Text>
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
+      {image && (
+        <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+      )}
     </View>
   );
 };
